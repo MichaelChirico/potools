ff884008e05e2fcc06e632c60c2bd4cc9e95ebec
Michael Chirico <michaelchirico4@gmail.com>
2021-01-03 00:15:45 -0500

    add first test package & first mock run-through without error (#13)
    * add first test package & first mock run-through without error
    
    * lost work somehow

---
+++ tests/testthat/helpers.R
@@ -0,0 +1,19 @@
+# copy a package to tmp, then overwrite any changes on exit
+restore_package <- function(dir, expr, tmp_conn) {
+  # this is way uglier than it should be. i'm missing something.
+  tdir <- tempdir()
+
+  file.copy(dir, tdir, recursive = TRUE)
+  on.exit({
+    unlink(dir, recursive = TRUE)
+    dir.create(dir)
+    file.copy(file.path(tdir, basename(dir)), dirname(dir), recursive = TRUE)
+    unlink(file.path(tdir, basename(dir)), recursive = TRUE)
+  })
+
+  if (!missing(tmp_conn)) {
+    old = options("__potools_testing_prompt_connection__" = tmp_conn)
+    on.exit(options(old), add = TRUE)
+  }
+  expr
+}

27a9b6194bca0a16158367e89805d1cb97edc4ce
Michael Chirico <michaelchirico4@gmail.com>
2021-05-25 00:01:45 -0700

    add duplicate messages to test package

--- tests/testthat/test_packages/r_non_template/R/foo.R
+++ tests/testthat/test_packages/r_non_template/R/foo.R
@@ -12,3 +12,13 @@ c <- function(x) {
 		ngettext(length(x), "small fail\n", "big fail\n")
 	))
 }
+
+# add some duplication of the messages to stress test logic
+#   on handling duplicates; part of #49
+d <- function(x) {
+  stop("I warned you!")
+}
+
+e <- function(x) {
+  stop("You failed", length(x), " times.")
+}

21f029bd017e4f65e6adafd2a8ce34ff8e18851f
Michael Chirico <michaelchirico4@gmail.com>
2021-05-28 22:51:21 -0700

    switch test package of coverage msg to actually be run

--- tests/testthat/test_packages/r_src_c/R/foo.R
+++ tests/testthat/test_packages/r_src_c/R/foo.R
@@ -1,4 +1,7 @@
 f <- function(x) {
+  # coverage: check_untranslated_cat skips this because of the second argument.
+  #   this lone cat in the package passes all but the last filter.
+  cat("a message", gettext("but this one's translated"))
   message("a string")
   x+1
 }

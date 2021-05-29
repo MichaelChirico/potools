229dd24e9d78c348de954483d5aab7203e500856
Michael Chirico <michaelchirico4@gmail.com>
2021-05-28 20:10:09 -0700

    annotate tests for posterity

--- tests/testthat/test_packages/r_cat_msg/R/foo.R
+++ tests/testthat/test_packages/r_cat_msg/R/foo.R
@@ -1,21 +1,21 @@
 function(x) {
-  base::cat("I warned you!", fill=TRUE, append=TRUE)
+  base::cat("I warned you!", fill=TRUE, append=TRUE) # (1)
   x+1
 }
 
 function(x) {
   # check that the newline is shown as "\n" (i.e., not a line return), #57
-  cat("Oh no", "you\ndon't!")
+  cat("Oh no", "you\ndon't!") # (2)
   sep = ""
-  cat("Miss me", "with", "this", sep=sep)
+  cat("Miss me", "with", "this", sep=sep) # <unmarked: don't know sep statically>
 }
 
 function(x) {
-  cat("Hi there", file=tempfile())
-  cat("Hi", "boss", sep="xx")
+  cat("Hi there", file=tempfile()) # <unmarked: don't translate cat to file>
+  cat("Hi", "boss", sep="xx") # (3)
 }
 
 function(x) {
-  cat(gettextf("shouldn't be translated"))
-  cat("This costs", x, "dollars")
+  cat(gettextf("shouldn't be translated")) # <cat is unmarked: gettextf is done
+  cat("This costs", x, "dollars") # (4)
 }

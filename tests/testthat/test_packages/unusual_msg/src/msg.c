675d46a613121baf33d9507e9cd00b5efca371a3
Michael Chirico <michaelchirico4@gmail.com>
2021-06-06 22:44:13 -0700

    coverage

--- tests/testthat/test_packages/unusual_msg/src/msg.c
+++ tests/testthat/test_packages/unusual_msg/src/msg.c
@@ -11,5 +11,13 @@ void hello_world(SEXP x) {
   // char array containing a comment that shouldn't be treated as such
   Rprintf(_("/* this is what a C comment looks like */"));
   Rprintf(_("// this is what a C comment looks like"));
+  // a very long message to test string wrapping
+  // TODO(#78): more thorough testing of this behavior
+  Rprintf(_(
+    "01234567890123456789.01234567890123456789"
+    "01234567890123456789.01234567890123456789"
+    "01234567890123456789.01234567890123456789"
+    "01234567890123456789.01234567890123456789"
+  ));
   return;
 }

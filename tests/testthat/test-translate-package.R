d9f046008a8e94b4aadc6dbcddd10eb5e264eb1d
Michael Chirico <michaelchirico4@gmail.com>
2021-06-09 00:16:25 -0700

    first pass at fixing whitespace trimming

--- tests/testthat/test-translate-package.R
+++ tests/testthat/test-translate-package.R
@@ -273,32 +273,41 @@ test_that("Various edge cases in retrieving/outputting messages in R files are h
     {
       translate_package(pkg, diagnostics = NULL)
 
-      pot_lines <- readLines(file.path(pkg, "po", "R-rMsgUnusual.pot"))
+      r_pot_files <- readLines(file.path(pkg, "po", "R-rMsgUnusual.pot"))
+      src_pot_files <- readLines(file.path(pkg, "po", "rMsgUnusual.pot"))
 
-      # raw strings edge cases
+      # (1) raw strings edge cases
+      # (2) whitespace trimming behavior (trim for R singular, don't for R plural)
       expect_all_match(
-        pot_lines,
-        c('msgid "\'abc\'"', 'msgid "\\"def\\""', 'msgid "R(\'abc\')"', 'msgid "r(\\"def\\")"', 'msgid "ghi"'),
+        r_pot_files,
+        c('msgid "\'abc\'"', 'msgid "\\"def\\""', 'msgid "R(\'abc\')"', 'msgid "r(\\"def\\")"', 'msgid "ghi"',
+          'good %s', 'msgid "singular "'),
         fixed = TRUE
       )
 
       # skip empty strings
-      expect_all_match(paste(pot_lines, collapse = "\n"), 'msgid ""\nmsgstr ""\n\n', fixed = TRUE, invert = TRUE)
+      expect_all_match(paste(r_pot_files, collapse = "\n"), 'msgid ""\nmsgstr ""\n\n', fixed = TRUE, invert = TRUE)
 
       # don't collapse strings in similar node positions across files
-      expect_all_match(pot_lines, c('msgid "copy one"', 'msgid "copy two"'))
+      expect_all_match(r_pot_files, c('msgid "copy one"', 'msgid "copy two"'))
 
       # ordering within the file
-      expect_true(which(pot_lines == 'msgid "first"') < which(pot_lines == 'msgid "second"'))
-      expect_true(which(pot_lines == 'msgid "second"') < which(pot_lines == 'msgid "third"'))
-      expect_true(which(pot_lines == 'msgid "third"') < which(pot_lines == 'msgid "fourth"'))
+      expect_true(which(r_pot_files == 'msgid "first"') < which(r_pot_files == 'msgid "second"'))
+      expect_true(which(r_pot_files == 'msgid "second"') < which(r_pot_files == 'msgid "third"'))
+      expect_true(which(r_pot_files == 'msgid "third"') < which(r_pot_files == 'msgid "fourth"'))
 
       # escaping/unescaping
       expect_all_match(
-        pot_lines,
+        r_pot_files,
         c('msgid "\\\\n vs \\n is OK"', 'msgid "\\\\t vs \\t is OK"',
           'msgid "strings with \\"quotes\\" are OK"', 'msgid "strings with escaped \\"quotes\\" are OK"'),
         fixed = TRUE
+      )
+
+      # whitespace trimming in C
+      expect_all_match(
+        src_pot_files,
+        c('looks like */ "', 'looks like %s "')
       )
     }
   )
--- tests/testthat/test_packages/unusual_msg/src/msg.c
+++ tests/testthat/test_packages/unusual_msg/src/msg.c
@@ -9,9 +9,10 @@ void hello_world(SEXP x) {
   // odd-ish spacing in implicit-cat char arrays, and implicit cat ending with macro
   Rprintf(_("that's a mighty big %"  PRId64"-sized wall over %"PRIu64), 100LL, 10L);
 
-  // char array containing a comment that shouldn't be treated as such
-  Rprintf(_("/* this is what a C comment looks like */"));
-  Rprintf(_("// this is what a C comment looks like"));
+  // char array containing a comment that shouldn't be treated as such; also trailing whitespace
+  Rprintf(_("/* this is what a C comment looks like */ "));
+  // also testing trailing whitespace action
+  Rprintf(_("// this is what a C comment looks like %s "), "abc");
 
   // a very long message to test string wrapping
   // TODO(#78): more thorough testing of this behavior

01b7f1daad96e1659ce2c661435400377eb8153b
Michael Chirico <michaelchirico4@gmail.com>
2021-06-17 23:34:06 -0700

    get msgid_plural basics working

--- R/get_src_messages.R
+++ R/get_src_messages.R
@@ -204,7 +204,8 @@ get_file_src_messages = function(file, translation_macros = c("_", "N_")) {
       .(
         fname, call_start, paren_start, paren_end,
         array_start,
-        msgid_plural = as.list(safe_substring(contents, array_start + 1L, array_end - 1L)),
+        msgid = safe_substring(contents, array_start + 1L, array_end - 1L),
+        msgid_plural = list(),
         call = safe_substring(contents, call_start, paren_end)
       )
     ],
@@ -212,6 +213,7 @@ get_file_src_messages = function(file, translation_macros = c("_", "N_")) {
       (!singular_array_idx),
       .(
         array_start = array_start[1L],
+        msgid = NA_character_,
         msgid_plural = build_msgid_plural(.BY$fname, .BY$paren_start, .BY$paren_end, array_start, array_end, contents),
         call = safe_substring(contents, .BY$call_start, .BY$paren_end)
       ),
@@ -328,36 +330,38 @@ build_msgid = function(left, right, starts, ends, contents) {
 }
 
 build_msgid_plural = function(fun, left, right, starts, ends, contents) {
+  if (!length(starts)) return(list())
+
   grout = get_grout(left, right, starts, ends, contents)
   target_arg = MESSAGE_CALLS[.(fun), str_arg]
 
-  browser()
   if (is.na(target_arg)) {
-    msgid_plural = list()
-    array_i = 1L
+    msgid_plural = character()
     msgid = ''
     for (ii in 2:length(grout)) {
-      msgid = paste0(msgid, substring(contents, starts[ii-1L], ends[ii-1L]))
+      msgid = paste0(msgid, substring(contents, starts[ii-1L]+1L, ends[ii-1L]-1L))
       if (grepl(',', grout[ii], fixed = TRUE)) {
-        msgid_plural[[array_i]] = msgid
-        array_i = array_i + 1L
+        msgid_plural = c(msgid_plural, msgid)
         msgid = ''
       }
     }
-    msgid_plural[[array_i]] = msgid
+    msgid_plural = c(msgid_plural, msgid)
   } else {
-    arg_i = 0L
-    msgid = ''
-    ii = 2L
-    while (arg_i < target_arg) {
-      msgid = paste0(msgid, substring(contents, starts[ii-1L], ends[ii-1L]))
+    arg_i = 1L
+    ii = 1L
+    while (arg_i != target_arg) {
+      if (arg_i > target_arg) stop(sprintf("Appear to have jumped past the target argument: %s%s", fun, substring(contents, left, right)))
+      # bold assumption: no nested commas. let's see if it works out...
       commas = gregexpr(",", grout[ii], fixed=TRUE)[[1L]]
       if (commas[1L] > 0L) arg_i = arg_i + length(commas)
       ii = ii + 1L
     }
-    msgid_plural = list(msgid)
+    jj = ii
+    while (jj <= length(grout) && !grepl(",", grout[jj], fixed = TRUE)) { jj = jj + 1L }
+    msgid = paste(substring(contents, starts[ii:jj - 1L]+1L, ends[ii:jj - 1L]-1L), collapse = '')
+    msgid_plural = msgid
   }
-  return(msgid_plural)
+  return(list(msgid_plural))
 }
 
 build_msgid_core = function(starts, ends, grout, contents) {

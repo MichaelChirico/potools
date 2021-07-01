688d899b225cfdb0a10a9ef3b2bd50871e84b948
Michael Chirico <michaelchirico4@gmail.com>
2021-06-30 22:36:50 -0700

    skip tests when gettext unavailable

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,7 @@
 library(testthat)
 library(potools)
 
-test_check("potools")
+# Failed on Solaris because the command-line tools are missing there (which means tools doesn't work there), #186
+if (isTRUE(check_potools_sys_reqs())) {
+  test_check("potools")
+}

4331f6b95ae038b88336bad4e5f61b418e2b75c7
Michael Chirico <michaelchirico4@gmail.com>
2021-07-11 18:20:28 -0700

    fine-tuning .pot output (#215)

--- tests/testthat/test-write-po-file.R
+++ tests/testthat/test-write-po-file.R
@@ -1,11 +1,23 @@
 # TODO: a better job here. I am really unmotivated to write these at the moment. Should have started from here...
 test_that("po_metadata constructor & methods work", {
-  metadata <- po_metadata(package = 'test', version = '0.0.1', language='da')
+  metadata <- po_metadata(
+    package = 'test', version = '0.0.1', language='da',
+    copyright = ''
+  )
   expect_s3_class(metadata, "po_metadata")
 
   expect_match(format(metadata), '"Project-Id-Version: test 0.0.1\\n"', fixed=TRUE)
-  expect_match(format(metadata, template=TRUE), '"Language-Team: LANGUAGE <LL@li.org>\\n"', fixed=TRUE)
-  expect_match(format(metadata, template=TRUE), '"Content-Type: text/plain; charset=CHARSET\\n"', fixed=TRUE)
+  expect_all_match(
+    format(metadata, template = TRUE),
+    c(
+      '"Language-Team: LANGUAGE <LL@li.org>\\n"',
+      '"Content-Type: text/plain; charset=CHARSET\\n"',
+      '# SOME DESCRIPTIVE TITLE.',
+      '# This file is distributed under the same license as the R package.',
+      '# FIRST AUTHOR <EMAIL@ADDRESS>, YEAR.'
+    ),
+    fixed=TRUE
+  )
   expect_match(format(metadata, use_plurals=TRUE), '"Plural-Forms: nplurals=2; plural=(n!=1);\\n"', fixed=TRUE)
 
   expect_output(print(metadata), '"Project-Id-Version: test 0.0.1\\n"', fixed=TRUE)

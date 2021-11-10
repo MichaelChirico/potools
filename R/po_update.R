6fd0c3026944b9edc53bdba1109aecbda5a8af3f
Hadley Wickham <h.wickham@gmail.com>
2021-11-10 08:00:01 -0600

    More docs about updating

--- R/po_update.R
+++ R/po_update.R
@@ -1,9 +1,29 @@
 #' Update all `.po` files with changes in `.pot`
 #'
+#' @description
 #' `po_update()` updates existing `.po` file after the `.pot` file has changed.
-#' The translations for existing messages are preserved; new messages are added;
-#' and translations for deleted message are marked as deprecated and moved to
-#' the bottom of the file.
+#' There are four cases:
+#'
+#' * New messages are added with blank `msgstr`.
+#'
+#' * Delete messages are marked as deprecated and moved to the bottom
+#'   of the file.
+#'
+#' * Major changes to existing messages will appear as a addition and a
+#'   deletion.
+#'
+#' * Minor changes will be preserved and flagged as fuzzy:
+#'
+#'     ```
+#'     #, fuzzy, c-format
+#'     #| msgid "Generating en@quot translations"
+#'     msgid "Updating '%s' %s translation"
+#'     msgstr "en@quot翻訳生成中。。。"
+#'     ```
+#'
+#'     The old message is given after the `#|`. Translators need to update the
+#'     message as necessary, then delete the old translation and the `fuzzy`
+#'     comment.
 #'
 #' @inheritParams po_extract
 #' @param lazy If `TRUE`, only `.po` files that are older than their

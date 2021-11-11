2cafc61cdd2cb98b09ac21ee26c5f501e8c3fa1b
Hadley Wickham <h.wickham@gmail.com>
2021-11-11 09:22:34 -0600

    Don't write ~ backup files

--- tests/testthat/test-po_update.R
+++ tests/testthat/test-po_update.R
@@ -10,3 +10,17 @@ test_that("user is told what's happening", {
     transform = standardise_dots
   )
 })
+
+test_that("user is told what's happening", {
+  temp <- local_test_package("R/test.r" = "message('Hello')")
+  withr::local_dir(temp)
+
+  po_extract()
+  po_create("fr")
+
+  writeLines("message('Hi')", file.path(temp, "R/test.R"))
+  po_extract()
+  po_update()
+
+  expect_false(file.exists(file.path(temp, "po/R-fr.po~")))
+})

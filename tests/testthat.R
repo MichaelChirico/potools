d61886b08a8b2e863de7bdd411515ca7f7692c8d
Michael Chirico <michaelchirico4@gmail.com>
2022-01-05 23:20:36 -0800

    turn off warn=2

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,7 +2,6 @@ library(testthat)
 library(potools)
 
 # Failed on Solaris because the command-line tools are missing there (which means tools doesn't work there), #186
-options(warn=2)
 if (isTRUE(check_potools_sys_reqs())) {
   test_check("potools")
 } else {

7d130b7d0d947d3f7736ad63deb057924099a66e
chiricom <chiricom@google.com>
2026-08-18 18:08:52 +0000

    drop extra branch again

--- R/get_r_messages.R
+++ R/get_r_messages.R
@@ -130,13 +130,10 @@ get_r_messages <- function(dir, custom_translation_functions = NULL, is_base = F
       res[single_mask] = substr(lines_sub, col1[single_mask], col2[single_mask])
     }
 
-    if (any(!single_mask)) {
-      multi_idx = which(!single_mask)
-      for (i in multi_idx) {
-        l1 = line1[i]; c1 = col1[i]; l2 = line2[i]; c2 = col2[i]
-        cm = if (nrow(file_comm)) file_comm[line1 >= l1 & line1 <= l2] else file_comm[0L]
-        res[i] = build_call(flines, cm, list(line1 = l1, col1 = c1, line2 = l2, col2 = c2))
-      }
+    for (i in which(!single_mask)) {
+      l1 = line1[i]; c1 = col1[i]; l2 = line2[i]; c2 = col2[i]
+      cm = if (nrow(file_comm)) file_comm[line1 >= l1 & line1 <= l2] else file_comm[0L]
+      res[i] = build_call(flines, cm, list(line1 = l1, col1 = c1, line2 = l2, col2 = c2))
     }
     res
   }, by = file]
